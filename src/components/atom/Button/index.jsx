d4410dc28112b3d993657c5abe843c11bc213e46
Danu Prasetyo <mas.danu29@gmail.com>
2023-06-20 13:18:31 +0700

    event-handler

--- src/components/atom/Button/index.jsx
+++ src/components/atom/Button/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function Button(props) {
-  const { color = "bg-black", children, size } = props;
-  return <button className={`h-15 py-1 px-6 font-semibold rounded-md ${color} ${size} text-white`}>{children}</button>;
+  const { color = "bg-black", children, size, type = "submit", onClick = () => {} } = props;
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`h-15 py-1 px-6 font-semibold rounded-md ${color} ${size} text-white`}
+    >
+      {children}
+    </button>
+  );
 }
--- src/components/organism/Login/index.jsx
+++ src/components/organism/Login/index.jsx
@@ -3,11 +3,30 @@ import InputForm from "../../molecules/InputForm";
 import Button from "../../atom/Button";
 
 export default function Login() {
+  const handleLogin = (event) => {
+    //     event.preventDefault(); = buat mencegah haalaman merefresh
+    event.preventDefault();
+    // console.log yg ke 1
+    console.log("terklik login");
+    //  event.target.email.value buat ngambil value yang ada di input form
+    console.log(event.target.email.value);
+    console.log(event.target.password.value);
+    // buat nyimpen data dari form input ke penyimpanan browser (aplications)
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
+    // buat ngeridairek ke halaman laen
+    window.location.href = "/products";
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleLogin}>
       <InputForm label="Email" name="email" type="email" placeholder="Masukkan email" />
       <InputForm label="password" type="password" name="password" placeholder="Masukkan password" />
-      <Button color="bg-blue-500" size="w-full">
+      {/* ini adalah eventhandler untuk nampilin console.log yg ke 1
+      <Button onClick={handleLogin} color="bg-blue-500 hover:bg-blue-700" size="w-full">
+        Login
+      </Button> */}
+      <Button color="bg-blue-500 hover:bg-blue-700" size="w-full">
         Login
       </Button>
     </form>
